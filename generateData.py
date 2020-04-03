339e63b3292a695ba5e875ae62e7564c6a65c1e8
JorinHakai <jorin.weatherston@hakai.org>
2020-04-02 17:46:50 -0700

    removed comments

--- generateData.py
+++ generateData.py
@@ -38,21 +38,5 @@ for filename in os.listdir(os.getcwd()):
   writeDataToFile('deaths_chart.csv', deaths_chart)
   writeDataToFile('recovered_chart.csv', recovered_chart)
 
-      # Access the general and chart-specific properties from the csv data.
-      # health_authority = row['health_authority']
-      # report_date = row['report_date']
-      # total_cases = row['total_cases']
-      # new_cases = row['new_cases']
-      # ever_hospitalized = row['ever_hospitalized']
-      # deaths = row['deaths']
-      # recovered = row['recovered']
-
-      # Write the headers and data to the fileName
-      # Total cases chart
-      # total_cases_list.append([report_date, ])
-      # new_cases_list.append()
-      # ever_hospitalized_list.append()
-      # deaths_list.append()
-      # recovered_list.append()
 
     
